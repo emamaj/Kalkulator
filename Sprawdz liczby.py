cc4ade433d0ff5a967aeb92455b2046dd0740f72
emamaj <edyta_m@hotmail.com>
2019-05-12 21:43:00 +0200

    kalk 4

---
+++ Sprawdz liczby.py
@@ -0,0 +1,10 @@
+print("Sprawdz czy podana liczba jest wieksza")
+a = int(input("Wprowadz pierwsza liczbe: "))
+b = int(input("Wprowadz druga liczbe: "))
+
+if a > b:
+    print("a jest wieksze od b")
+if a < b:
+    print("a jest mniejsze od b")
+if a == b:
+    print("a jest rowne b")
