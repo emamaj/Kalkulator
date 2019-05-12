09e4e397bef189fb99868bccb3d5a4b6a96602a0
emamaj <edyta_m@hotmail.com>
2019-05-12 20:05:21 +0200

    kalk one

---
+++ kalkulator.py
@@ -0,0 +1,16 @@
+print("Prosty kalkulator")
+a = int(input("Podaj pierwsza liczbe: "))
+b = int(input("Podaj druga liczbe: "))
+c = int(input("Wybierz rodzaj dzialania: 1 - dodawanie, 2 - odejmowanie, 3 - mnozenie, 4 - dzielenie: "))
+if (c == 1):
+    wynik = a + b
+elif (c == 2):
+    wynik = a - b
+elif ( c == 3):
+    wynik = a * b
+elif(c == 4):
+    wynik = a / b
+else:
+    print("Dodawanie zlego wyboru!")
+
+print("Wynik dzialania to: ", wynik)
