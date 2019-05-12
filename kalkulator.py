d937624a30ced507a9b224f9bac82c43c7bd8c7d
emamaj <edyta_m@hotmail.com>
2019-05-12 21:14:22 +0200

    kalk 3

--- kalkulator.py
+++ kalkulator.py
@@ -1,15 +1,18 @@
 print("Prosty kalkulator")
 a = int(input("Podaj pierwsza liczbe: "))
 b = int(input("Podaj druga liczbe: "))
-c = int(input("Wybierz rodzaj dzialania: 1 - dodawanie, 2 - odejmowanie, 3 - mnozenie, 4 - dzielenie: "))
+c = int(input("Wybierz rodzaj dzialania: 1 - dodawanie, 2 - odejmowanie, 3 - mnozenie, 4 - dzielenie, 5 - potegowanie: "))
 if (c == 1):
     wynik = a + b
 elif (c == 2):
     wynik = a - b
-elif ( c == 3):
+elif (c == 3):
     wynik = a * b
-elif(c == 4):
+elif (c == 4):
     wynik = a / b
+elif (c == 5):
+    wynik = a ** b
+
 else:
     print("Dodawanie zlego wyboru!")
 
